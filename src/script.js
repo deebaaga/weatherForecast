0b83c1e0bde8d6b1a4e88e22f23133355f09f69f
deebaaga <deebagamushtaq@gmail.com>
2024-07-13 11:37:40 +0530

    added content in html,css,&js file
    added previous projects html,css&js to modify for forecast further

---
+++ src/script.js
@@ -0,0 +1,59 @@
+window.onload = function () {
+  let ctime = new Date();
+  let hour = ctime.getHours();
+  let mins = ctime.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[ctime.getDay()];
+
+  let currenttime = document.querySelector("#time");
+  currenttime.innerHTML = `${day} ${hour}:${mins} ,`;
+
+  function displayTemp(response) {
+    let temperature = Math.round(response.data.temperature.current);
+
+    let city = response.data.city;
+    console.log(response.city);
+
+    let description = response.data.condition.description;
+
+    let humidity = response.data.temperature.humidity;
+
+    let wind = response.data.wind.speed;
+
+    let iconUrl = response.data.condition.icon_url;
+
+    let tempp = document.querySelector("#degree");
+    tempp.innerHTML = `${temperature}`;
+
+    let placee = document.querySelector("#cityy");
+    placee.innerHTML = `${city}`;
+
+    let feelss = document.querySelector("#feels");
+    feelss.innerHTML = `${description}`;
+
+    let humidd = document.querySelector("#humid");
+    humidd.innerHTML = `${humidity}%`;
+
+    let windd = document.querySelector("#wind");
+    windd.innerHTML = `${wind} km/h`;
+
+    let iconn = document.querySelector("#icon");
+    iconn.innerHTML = `<img src="${iconUrl}" class="emoji" />`;
+  }
+  function cityinput(event) {
+    event.preventDefault();
+    let city = document.querySelector(".search-inp").value;
+    let apiKey = "ba24ot429e5023e68078f3c73b9cdf5d";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(displayTemp);
+  }
+  document.querySelector("form").addEventListener("submit", cityinput);
+};
